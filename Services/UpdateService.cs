8ce22a4ff1bf62dde0c2967ed51f091037ef57be
Pavel Milokum <pavelmilokum@gmail.com>
2025-11-28 14:46:48 +0300

    refactorings and tests

--- Services/UpdateService.cs
+++ Services/UpdateService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using System.Text;
 
+[assembly: InternalsVisibleTo("CGPAHTests")]
 namespace CommunigateAntispamHelper.Services
 {
 
